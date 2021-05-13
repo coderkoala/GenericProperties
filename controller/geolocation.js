246b0c7b34fad4b17c1cf477cc45ad82bb7087dc
iamtribulation@outlook.com <iamtribulation@outlook.com>
2021-05-14 00:25:08 +0545

    Query for polling agent radii done

--- controller/geolocation.js
+++ controller/geolocation.js
@@ -3,10 +3,12 @@
 const { QueryTypes } = require("sequelize");
 const db = require("../models");
 let DynamicsCrmRest = require("./src/dynamics");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 class homeController {
   async view(req, res, next) {
+    let newlat = req.body.latLong;
+
     // const users = await db.sequelize.query("SELECT * FROM `users`", {
     //   type: QueryTypes.SELECT,
     // }); Just test code.
@@ -14,22 +16,59 @@ class homeController {
   }
 
   async post(req, res, next) {
-    let leadid = undefined === req.body.location || req.body.location.match(/(^\s+$|^$)|((@|\||\*|\^|\_|%|!|~|\+)+)/i) ? "!invalid" : `(${req.body.location})`;
-    
+    let leadid =
+      undefined === req.body.location ||
+      req.body.location.match(/(^\s+$|^$)|((@|\||\*|\^|\_|%|!|~|\+)+)/i)
+        ? "!invalid"
+        : `(${req.body.location})`;
+
     let crm = new DynamicsCrmRest();
-    await crm.get(`leads${leadid}?$select=new_latitude,new_longitude,subject,new_fullname`).then((result) => {
-      if (undefined !== result.data) {
-        result.data.hotLink = process.env.dynamics_crm_record_link.replace('{entity}', 'lead') + result.data.leadid;
-        res.json(result.data);
-      } else {
-        res
-          .status(404)
-          .send({
+    await crm
+      .get(
+        `leads${leadid}?$select=new_latitude,new_longitude,subject,new_fullname`
+      )
+      .then((result) => {
+        if (undefined !== result.data) {
+          result.data.hotLink =
+            process.env.dynamics_crm_record_link.replace("{entity}", "lead") +
+            result.data.leadid;
+          res.json(result.data);
+        } else {
+          res.status(404).send({
             error:
               "No lead records found. Please try again with a valid record.",
           });
-      }
-    });
+        }
+      });
+  }
+
+  async fetchAgents(req, res, next) {
+    let latitude = this.latitude;
+    let longitude = this.longitude;
+    let query = `SELECT 
+                id, address, name, 
+                CONCAT(ST_X(coordinates), ',' ,ST_Y(coordinates)) AS latLong,
+                (6371 * ACOS(COS(RADIANS(${latitude})) * COS(RADIANS(ST_Y(coordinates))) 
+                * COS(RADIANS(ST_X(coordinates)) - RADIANS(${longitude})) + SIN(RADIANS(${latitude}))
+                * SIN(RADIANS(ST_Y(coordinates))))) AS distance
+                FROM geolocation
+                WHERE MBRContains
+                    (
+                    LineString
+                        (
+                        Point (
+                            ${longitude} + 30 / (111.320 * COS(RADIANS(${latitude}))),
+                            ${latitude} + 30 / 111.133
+                        ),
+                        Point (
+                            ${longitude} - 30 / (111.320 * COS(RADIANS(${latitude}))),
+                            ${latitude} - 30 / 111.133
+                        )
+                    ),
+                    coordinates
+                    )
+                HAVING distance < 5
+                ORDER By distance`;
   }
 }
 
