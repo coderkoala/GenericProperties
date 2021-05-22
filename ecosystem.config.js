6b8b9fe431bedc68f87c2f617b3d5639482afc78
iamtribulation@outlook.com <iamtribulation@outlook.com>
2021-05-22 09:35:28 +0545

    Dev - tweaked pm2 production settings
    * Explicit log location for easy cleanup
    * Stop hotreload for specific folders:
        - logs, node_modules, public
    * Removed node_cache in favor of persistent storage based on life
    * TODO: crontab script for cleanup (24 hour autoit)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,26 @@
 module.exports = {
-  apps : [{
-    script: './bin/www',
-    watch: './'
-  }],
+  apps: [
+    {
+      script: "./bin/www",
+      ignore_watch: ["logs", "node_modules", "public"],
+      watch: "./",
+      "log": "./logs/server.log",
+      "log_type": "json",
+      "log_date_format": "DD-MM-YYYY hh:mm"
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
 };
