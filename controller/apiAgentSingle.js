b191e3a14bf48326a35da5b042be1db6c779a478
iamtribulation@outlook.com <iamtribulation@outlook.com>
2021-05-24 22:10:05 +0545

    Tweak - made the render 2 fold, can spit out both json or web view for appending depending on query param

--- controller/apiAgentSingle.js
+++ controller/apiAgentSingle.js
@@ -1,19 +1,23 @@
 "use strict";
 let DynamicsCrmRest = require("./src/dynamics");
+let view = require("./src/agentViewHelper");
 require("dotenv").config();
 
 class agentsController {
   async fetchAgentTuple(req, res) {
+    let renderAsTable = req.query.table || true;
     let agentid =
-      undefined === req.query.dynamics_id ||
-      null === req.query.dynamics_id ||
-      req.query.dynamics_id.match(/(^\s+$|^$)|((@|\||\*|\^|\_|%|!|~|\+)+)/i)
+      undefined === req.query.id ||
+      null === req.query.id ||
+      req.query.id.match(/(^\s+$|^$)|((@|\||\*|\^|\_|%|!|~|\+)+)/i)
         ? `^invalid`
-        : `(${req.query.dynamics_id})`;
+        : `(${req.query.id})`;
 
     if ("^invalid" === agentid) {
       res.status(400).send({
-        error: "No agent found with that UUID.",
+        title: "Failed retrieval",
+        message: "Invalid UUID was provided. Please provide a valid UUID.",
+        icon: "error",
       });
       return;
     }
@@ -25,12 +29,25 @@ class agentsController {
           process.env.dynamics_crm_record_link.replace(
             "{entity}",
             "cr4f2_agentsandrealtor"
-          ) + result.data.cr4f2_agentsandrealtorid || 'undefined';
-        res.json(result.data);
-      } else {
+          ) + result.data.cr4f2_agentsandrealtorid || "undefined";
+        if (renderAsTable) {
+          let viewInstance = new view();
+          res.status(200).json({title:result.data.cr4f2_fullname,data: viewInstance.outputSingleAgent(result.data) });
+          return;
+        } else {
+          res.json(result.data);
+        }
+      } else if (404=== result.response.status) {
         res.status(404).send({
-          error:
-            "No agent found with that UUID.",
+          title: "Failed retrieval",
+          message: "No agent found with that UUID.",
+          icon: "error",
+        });
+      } else {
+        res.status(400).send({
+          title: "Failed retrieval",
+          message: "The server was unable to process your request.",
+          icon: "error",
         });
       }
     });
