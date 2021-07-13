1cfc8d2b73e6924e210d479d37560a5c8dd4a0a4
iamtribulation@outlook.com <iamtribulation@outlook.com>
2021-07-14 00:32:48 +0545

    Retaining source through minification script

--- public/javascripts/wysiwyg.js
+++ public/javascripts/wysiwyg.js
@@ -1,1 +1,224 @@
-!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports):"function"==typeof define&&define.amd?define(["exports"],e):e(t.pell={})}(this,function(t){"use strict";var e=Object.assign||function(t){for(var e=1;e<arguments.length;e++){var n=arguments[e];for(var i in n){Object.prototype.hasOwnProperty.call(n,i)&&(t[i]=n[i])}}return t},n="defaultParagraphSeparator",i="formatBlock",r=function(t,e,n){return t.addEventListener(e,n)},o=function(t,e){return t.appendChild(e)},u=function(t){return document.createElement(t)},l=function(t){return document.queryCommandState(t)},a=function(t){var e=1<arguments.length&&void 0!==arguments[1]?arguments[1]:null;return document.execCommand(t,!1,e)},c={bold:{icon:"<b>B</b>",title:"Bold",state:function(){return l("bold")},result:function(){return a("bold")}},italic:{icon:"<i>I</i>",title:"Italic",state:function(){return l("italic")},result:function(){return a("italic")}},underline:{icon:"<u>U</u>",title:"Underline",state:function(){return l("underline")},result:function(){return a("underline")}},strikethrough:{icon:"<strike>S</strike>",title:"Strike-through",state:function(){return l("strikeThrough")},result:function(){return a("strikeThrough")}},heading1:{icon:"<b>H</b>",title:"Heading",result:function(){return a(i,"<h1>")}},heading2:{icon:"<b>H<sub>2</sub></b>",title:"Heading 2",result:function(){return a(i,"<h2>")}},paragraph:{icon:"&#182;",title:"Paragraph",result:function(){return a(i,'<p style="margin: 0; font-size: 14px; line-height: 1.8; word-break: break-word; text-align: justify; mso-line-height-alt: 25px; margin-top: 0; margin-bottom: 0;">')}},quote:{icon:"&#8220; &#8221;",title:"Quote",result:function(){return a(i,"<blockquote>")}},olist:{icon:"&#35;",title:"Ordered List",result:function(){return a("insertOrderedList")}},ulist:{icon:"&#8226;",title:"Unordered List",result:function(){return a("insertUnorderedList")}},code:{icon:"&lt;/&gt;",title:"Code",result:function(){return a(i,"<pre>")}},line:{icon:"&#8213;",title:"Horizontal Line",result:function(){return a("insertHorizontalRule")}},link:{icon:"&#128279;",title:"Link",result:function(){var t=window.prompt("Enter the link URL");t&&a("createLink",t)}},image:{icon:"&#128247;",title:"Image",result:function(){var t=window.prompt("Enter the image URL");t&&a("insertImage",t)}}},s={actionbar:"pell-actionbar",button:"pell-button",content:"pell-content",selected:"pell-button-selected"},d=function(t){var l=t.actions?t.actions.map(function(t){return"string"==typeof t?c[t]:c[t.name]?e({},c[t.name],t):t}):Object.keys(c).map(function(t){return c[t]}),d=e({},s,t.classes),f=t[n]||"div",m=u("div");m.className=d.actionbar,o(t.element,m);var p=t.element.content=u("div");return p.contentEditable=!0,p.className=d.content,p.oninput=function(e){var n=e.target.firstChild;n&&3===n.nodeType?a(i,"<"+f+">"):"<br>"===p.innerHTML&&(p.innerHTML=""),t.onChange(p.innerHTML)},p.onkeydown=function(t){var e;"Enter"===t.key&&"blockquote"===(e=i,document.queryCommandValue(e))&&setTimeout(function(){return a(i,"<"+f+' style="margin: 0; font-size: 14px; line-height: 1.8; word-break: break-word; text-align: justify; mso-line-height-alt: 25px; margin-top: 0; margin-bottom: 0;">')},0)},o(t.element,p),l.forEach(function(t){var e=u("button");if(e.className=d.button,e.innerHTML=t.icon,e.title=t.title,e.setAttribute("type","button"),e.onclick=function(){return t.result()&&p.focus()},t.state){var n=function(){return e.classList[t.state()?"add":"remove"](d.selected)};r(p,"keyup",n),r(p,"mouseup",n),r(e,"click",n)}o(m,e)}),t.styleWithCSS&&a("styleWithCSS"),a(n,f),t.element},f={exec:a,init:d};t.exec=a,t.init=d,t.default=f,Object.defineProperty(t,"__esModule",{value:!0})});
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.pell = {})));
+}(this, (function (exports) { 'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var defaultParagraphSeparatorString = 'defaultParagraphSeparator';
+var formatBlock = 'formatBlock';
+var addEventListener = function addEventListener(parent, type, listener) {
+  return parent.addEventListener(type, listener);
+};
+var appendChild = function appendChild(parent, child) {
+  return parent.appendChild(child);
+};
+var createElement = function createElement(tag) {
+  return document.createElement(tag);
+};
+var queryCommandState = function queryCommandState(command) {
+  return document.queryCommandState(command);
+};
+var queryCommandValue = function queryCommandValue(command) {
+  return document.queryCommandValue(command);
+};
+
+var exec = function exec(command) {
+  var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+  return document.execCommand(command, false, value);
+};
+
+var defaultActions = {
+  bold: {
+    icon: '<b>B</b>',
+    title: 'Bold',
+    state: function state() {
+      return queryCommandState('bold');
+    },
+    result: function result() {
+      return exec('bold');
+    }
+  },
+  italic: {
+    icon: '<i>I</i>',
+    title: 'Italic',
+    state: function state() {
+      return queryCommandState('italic');
+    },
+    result: function result() {
+      return exec('italic');
+    }
+  },
+  underline: {
+    icon: '<u>U</u>',
+    title: 'Underline',
+    state: function state() {
+      return queryCommandState('underline');
+    },
+    result: function result() {
+      return exec('underline');
+    }
+  },
+  strikethrough: {
+    icon: '<strike>S</strike>',
+    title: 'Strike-through',
+    state: function state() {
+      return queryCommandState('strikeThrough');
+    },
+    result: function result() {
+      return exec('strikeThrough');
+    }
+  },
+  heading1: {
+    icon: '<b>H<sub>1</sub></b>',
+    title: 'Heading 1',
+    result: function result() {
+      return exec(formatBlock, '<h1>');
+    }
+  },
+  heading2: {
+    icon: '<b>H<sub>2</sub></b>',
+    title: 'Heading 2',
+    result: function result() {
+      return exec(formatBlock, '<h2>');
+    }
+  },
+  paragraph: {
+    icon: '&#182;',
+    title: 'Paragraph',
+    result: function result() {
+      return exec(formatBlock, '<p>');
+    }
+  },
+  quote: {
+    icon: '&#8220; &#8221;',
+    title: 'Quote',
+    result: function result() {
+      return exec(formatBlock, '<blockquote>');
+    }
+  },
+  olist: {
+    icon: '&#35;',
+    title: 'Ordered List',
+    result: function result() {
+      return exec('insertOrderedList');
+    }
+  },
+  ulist: {
+    icon: '&#8226;',
+    title: 'Unordered List',
+    result: function result() {
+      return exec('insertUnorderedList');
+    }
+  },
+  code: {
+    icon: '&lt;/&gt;',
+    title: 'Code',
+    result: function result() {
+      return exec(formatBlock, '<pre>');
+    }
+  },
+  line: {
+    icon: '&#8213;',
+    title: 'Horizontal Line',
+    result: function result() {
+      return exec('insertHorizontalRule');
+    }
+  },
+  link: {
+    icon: '&#128279;',
+    title: 'Link',
+    result: function result() {
+      var url = window.prompt('Enter the link URL');
+      if (url) exec('createLink', url);
+    }
+  },
+  image: {
+    icon: '&#128247;',
+    title: 'Image',
+    result: function result() {
+      var url = window.prompt('Enter the image URL');
+      if (url) exec('insertImage', url);
+    }
+  }
+};
+
+var defaultClasses = {
+  actionbar: 'pell-actionbar',
+  button: 'pell-button',
+  content: 'pell-content',
+  selected: 'pell-button-selected'
+};
+
+var init = function init(settings) {
+  var actions = settings.actions ? settings.actions.map(function (action) {
+    if (typeof action === 'string') return defaultActions[action];else if (defaultActions[action.name]) return _extends({}, defaultActions[action.name], action);
+    return action;
+  }) : Object.keys(defaultActions).map(function (action) {
+    return defaultActions[action];
+  });
+
+  var classes = _extends({}, defaultClasses, settings.classes);
+
+  var defaultParagraphSeparator = settings[defaultParagraphSeparatorString] || 'div';
+
+  var actionbar = createElement('div');
+  actionbar.className = classes.actionbar;
+  appendChild(settings.element, actionbar);
+
+  var content = settings.element.content = createElement('div');
+  content.contentEditable = true;
+  content.className = classes.content;
+  content.oninput = function (_ref) {
+    var firstChild = _ref.target.firstChild;
+
+    if (firstChild && firstChild.nodeType === 3) exec(formatBlock, '<' + defaultParagraphSeparator + '>');else if (content.innerHTML === '<br>') content.innerHTML = '';
+    settings.onChange(content.innerHTML);
+  };
+  content.onkeydown = function (event) {
+    if (event.key === 'Enter' && queryCommandValue(formatBlock) === 'blockquote') {
+      setTimeout(function () {
+        return exec(formatBlock, '<' + defaultParagraphSeparator + '>');
+      }, 0);
+    }
+  };
+  appendChild(settings.element, content);
+
+  actions.forEach(function (action) {
+    var button = createElement('button');
+    button.className = classes.button;
+    button.innerHTML = action.icon;
+    button.title = action.title;
+    button.setAttribute('type', 'button');
+    button.onclick = function () {
+      return action.result() && content.focus();
+    };
+
+    if (action.state) {
+      var handler = function handler() {
+        return button.classList[action.state() ? 'add' : 'remove'](classes.selected);
+      };
+      addEventListener(content, 'keyup', handler);
+      addEventListener(content, 'mouseup', handler);
+      addEventListener(button, 'click', handler);
+    }
+
+    appendChild(actionbar, button);
+  });
+
+  if (settings.styleWithCSS) exec('styleWithCSS');
+  exec(defaultParagraphSeparatorString, defaultParagraphSeparator);
+
+  return settings.element;
+};
+
+var pell = { exec: exec, init: init };
+
+exports.exec = exec;
+exports.init = init;
+exports['default'] = pell;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
